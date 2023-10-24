c3ae02e568ea9a2adddf2910d44ae629af567c47
wilbyang <yang.wilby@gmail.com>
2023-10-25 00:58:31 +0200

    remove unused imports

--- libwy-lingo-api/src/main/java/com/libwy/lingo/api/repository/LingoJpaRepository.java
+++ libwy-lingo-api/src/main/java/com/libwy/lingo/api/repository/LingoJpaRepository.java
@@ -1,9 +1,6 @@
 package com.libwy.lingo.api.repository;
 
-import com.libwy.lingo.core.domain.Lingo;
 import com.libwy.lingo.core.repository.LingoRepository;
-
-import java.util.Optional;
 
 public interface LingoJpaRepository extends LingoRepository {
 }
